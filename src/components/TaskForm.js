62699c3651fc28a2d96ba2814f301a3a7f608954
tomasnapolitano <t97napolitano@gmail.com>
2022-09-16 11:39:41 -0300

    coded task adding and rendering, with uuid

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,11 +1,36 @@
 import '../styles/TaskForm.css'
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react'
 
-const TaskForm = () => {
+
+const TaskForm = ({ formSubmit }) => {
+
+    const [input, setInput] = useState()
+
+const handleChange = (e) => {
+    setInput(e.target.value)
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault() //prevenimos que se recargue al enviar el form
+    //agregamos la nueva tarea
+    const newTask = {
+        id: uuidv4(),
+        title: input,
+        completed: false
+    }
+    formSubmit(newTask)
+}
+
     return(
-        <div className='taskFormBox'>
-            <h1>Ingresar nueva Task:</h1>
-            <button>+</button>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" 
+            placeholder="Write your task!"
+            name="title"
+            onChange={handleChange}
+            />
+            <button>Add Task</button>
+        </form>
     )
 }
 
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,15 +1,26 @@
 import Task from './Task'
 import TaskForm from './TaskForm'
 import '../styles/TaskList.css'
+import { useState } from 'react'
 
 const tasks = [{id: 1, title: 'Consulta con otorrino'},
 {id:2, title:'Comprar regalos de Navidad'},
 {id:3, title:'Reservar mesa para aniversario'}]
 
 const TaskList = () => {
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = (task) => {
+        if(task.title.trim()){
+            task.title = task.title.trim();
+            const updatedTasks =[task, ...tasks];
+            setTasks(updatedTasks);
+        }
+    }
+
     return(
         <div className='listContainer'>
-            <TaskForm/>
+            <TaskForm formSubmit={addTask}/>
             {tasks.map((task)=>(<Task key={task.id}title={task.title}/>))}
         </div>
     )
